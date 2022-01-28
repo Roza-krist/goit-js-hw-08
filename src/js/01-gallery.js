da639f0647e36a4c38e831ca01edde4e82900677
Roza-krist <rozakristjuhan@gmail.com>
2022-01-28 11:39:06 +0200

    final version

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+  <a class="gallery__item" href='${original}'>
+    <img
+    loading = 'lazy'
+      class="gallery__image"
+      src='${preview}'
+      alt='${description}'
+    />
+  </a>
+</div>`,
+    )
+    .join('');
+}
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  //   swipeClose: true,
+  //   close: true,
+});
